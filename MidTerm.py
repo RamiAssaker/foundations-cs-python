07edb628eac96559bcf3cdf5a1850e99a575c3a4
RamiAssaker <ramiassaker@hotmail.com>
2023-11-10 20:08:55 +0200

    Defining close tab function

--- MidTerm.py
+++ MidTerm.py
@@ -5,24 +5,34 @@ def showMenu(): #Function to display the menu at the beginning
 def openTab(): #Function to display that a tab is opened
     open_tab = {}
     title = input("Enter the Title of the Website:\n")
-    for i in title:
+    for i in title: 
         if title[0].islower():
-            title1 = title[0].upper() + title[1:]
+            title1 = title[0].upper() + title[1:] #changing the first letter to capital incase it was lower case
     url = input("Enter the URL of the website:\n")
-    open_tab["Title"] = [title] #to add to the dict the "key" title with its value which is from the user input
-    open_tab["URL"] = [url] #to add to the dict the "key" URL with its value which is also from the user input
-    print("\n",title1,"Tab Opened")
-    #if we print the dictionary it will be like: {'Title': ['google'], 'URL': ['www.google.com']} 
-    
-    
+    open_tab[title] = [url] #to add to the dict the "key" title with its value which is from the user input
+    print("\n",title1,"tab opened")
+    print(open_tab)
+    #if we print the dictionary it will be: {'Title': ['google', 'google.com']} 
 
+#def closeTab():
+#    close = int(input("Which tab would you like to close:"))
+ #   if close == key in open_tab:
+  #      del open_tab[key]
+        
 
 def main():
     showMenu()
     choice = eval(input("Select from the above:\t"))#User input
+    order_of_tabs = []
     while choice != 7: 
         if choice == 1:
             openTab()
+            order_of_tabs.append(1)
+            #print(order_of_tabs) shows the list and numbers of tabs opened
+            
+       # elif choice == 2:
+        #    closeTab()
+            
             
         
     
