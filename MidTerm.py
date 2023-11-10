71447ef4f8b7ea1c7f06b04aa2d2398c0e6fee93
RamiAssaker <ramiassaker@hotmail.com>
2023-11-10 13:46:25 +0200

    Modifying the opentab fct

--- MidTerm.py
+++ MidTerm.py
@@ -2,13 +2,17 @@ def showMenu(): #Function to display the menu at the beginning
     print("Hello User!\n")
     print("1-\tOpen Tab\n" + "2-\tClose Tab\n" + "3-\tSwitch Tab\n" + "4-\tDisplay All Tabs\n" + "5-\tOpen Nested Tab\n" + "6-\tClear All Tabs\n" + "7-\tSave Tabs\n" + "8-\tImport Tabs\n" + "9-\tExit\n")
 
-def openTab():
+def openTab(): #Function to display that a tab is opened
     open_tab = {}
-    title = input("Enter the Title:\n")
+    title = input("Enter the Title of the Website:\n")
+    for i in title:
+        if title[0].islower():
+            title1 = title[0].upper() + title[1:]
     url = input("Enter the URL of the website:\n")
-    open_tab["Title"] = [title]
-    open_tab["URL"] = [url]
-    print("\n",title,"Tab Opened")
+    open_tab["Title"] = [title] #to add to the dict the "key" title with its value which is from the user input
+    open_tab["URL"] = [url] #to add to the dict the "key" URL with its value which is also from the user input
+    print("\n",title1,"Tab Opened")
+    #if we print the dictionary it will be like: {'Title': ['google'], 'URL': ['www.google.com']} 
     
     
 
