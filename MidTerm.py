5d879518f55916058098cbd3b6712c27d81b57a3
RamiAssaker <ramiassaker@hotmail.com>
2023-11-10 12:25:41 +0200

    Creating menu display

---
+++ MidTerm.py
@@ -0,0 +1,8 @@
+def showMenu():
+    print("Hello User!")
+    print("1-\tOpen Tab\n" + "2-\tClose Tab\n" + "3-\tSwitch Tab\n" + "4-\tDisplay All Tabs\n" + "5-\tOpen Nested Tab\n" + "6-\tClear All Tabs\n" + "7-\tSave Tabs\n" + "8-\tImport Tabs\n" + "9-\tExit\n")
+
+def main():
+    showMenu()
+    
+main()
