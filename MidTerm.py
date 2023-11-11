e2de285d84393bc94f36fafe964d88d8d5d3b43e
RamiAssaker <ramiassaker@hotmail.com>
2023-11-11 16:07:04 +0200

    Creating switch tab function

--- MidTerm.py
+++ MidTerm.py
@@ -1,3 +1,5 @@
+import requests #used for the "switch tab" case
+
 def showMenu(): #Function to display the menu at the beginning
     print("1-\tOpen Tab\n" + "2-\tClose Tab\n" + "3-\tSwitch Tab\n" + "4-\tDisplay All Tabs\n" + "5-\tOpen Nested Tab\n" + "6-\tClear All Tabs\n" + "7-\tSave Tabs\n" + "8-\tImport Tabs\n" + "9-\tExit\n")
 
@@ -17,16 +19,26 @@ def closeTab(open_dict):
     tabs_delete = [] #we need a list to insert the key we want to delete since we cannot delete from dictionary during iteration(error:dictionary changed size during iteration ) 
     for i in open_dict:
         if close == i:
-            tabs_delete.append(i)
+            tabs_delete.append(i) #append what is in index i to the list "tabs_delete"
         else:
-          tabs_delete.append(list(open_dict.keys())[-1])
-     
-    for j in tabs_delete:
-        if j in open_dict:
+          tabs_delete.append(list(open_dict.keys())[-1]) #transforming the dictionary into a list and appending to the list "tabs_delete" the last index of the dictionary
+    for j in tabs_delete: #iterating through the list
+        if j in open_dict: #double verification of what must be deleted, (if it is in the dictionary -->delete)
             del open_dict[j]
+        else:
+            print("No webpage found. Please insert an opened tab\n")
     print(open_dict)
-
-
+    
+def switchTab(open_dict):
+    switch = input("Which tab do you want to switch to:")
+    tab_switched = []
+    for i in open_dict:
+        if switch == i:
+            for value in open_dict.items():
+                response = requests.get(value)
+                print(response)
+            
+    
 def main():
     print("Hello User!\n")
     open_tab = {}
@@ -40,10 +52,14 @@ def main():
         if choice == 1:
             openTab(open_tab)
             order_of_tabs.append(1)
-            print(open_tab)
+            print(open_tab) #THIS MUST BE COMMENTED
             #print(order_of_tabs) shows the list and numbers of tabs opened
         elif choice == 2:
             closeTab(open_tab)
+        elif choice == 3:
+            switchTab(open_tab)
+        
+            
             
             
         
